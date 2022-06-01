87cd6c9c2ca4bba6006cd8e32fd7c68527567360
rbbetul <106395101+rbbetul@users.noreply.github.com>
2022-06-01 17:05:44 +0300

    Update nightlife_project.py

--- nightlife_project.py
+++ nightlife_project.py
@@ -7,142 +7,194 @@ Original file is located at
     https://colab.research.google.com/drive/1_vO2KG29h6V9Iqz7YMtG9QD6DdrP0gDq
 """
 
-#data with faker
-import subprocess
-import sys
-subprocess.check_call([sys.executable, "-m", "pip", "install", "wit"])
-subprocess.check_call([sys.executable, "-m", "pip", "install", "faker"])
-
+"""Installing the required packages"""
+!pip install pywit
+!pip install streamlit
+!pip install wit
 import random
-import faker
-
-import pandas as pd
-import datetime
-from faker import Faker
-fake_tr = Faker("tr_TR")
-fake_en = Faker()
-genress = ["r&b", "metal", "rock", "techno", "pop", "electronic", "blues", "jazz", "punk"]
-provinces = ["taksim", "beşiktaş", "kadıköy", "beyoğlu", "levent", "bebek"]
-province = []
-venue = []
-genreee = []
-for i in range(500):
-  venue.append(fake_en.domain_word())
-  genreee.append(random.choice(genress))
-  province.append(random.choice(provinces))
-
-import re
-"""
-f1 = open("artist_data.csv", "r")
-artist = []
-for celeb in f1:
-  celeb_pp = str(celeb).lower()
-  celeb_pp = re.sub("\n", "", celeb_pp)
-  artist.append(celeb_pp)
-artist[:10]
-
-artist_genre_dict[""]
-
-f2 = open("genre_data.csv", "r")
-genre = []
-for gen in f2:
-  genre_pp = str(gen).lower()
-  genre_pp = re.sub("\n", "", genre_pp)
-  genre.append(genre_pp)
-genre[:10]
-
-artist_genre_dict = dict(zip(artist, genre))
-"""
 from pandas._libs import index
-fake_data = {"genre": genreee, "province":province}
-nightlife_data = pd.DataFrame(fake_data, index=venue)
-
-nightlife_data[:20]
-
-
+import numpy as np
 from wit import Wit
+import re
+import pandas as pd
+import streamlit as st
+import datetime
+import random
+
+"""Our dataset of bars, their locations, genres they play and addresses"""
+nl_data = pd.read_csv("ling360 project FINAL_VER venues genres data - Sayfa1.csv")
+nl_data
+nightlife_data = nl_data.set_index('VENUE')
+nightlife_data
+
+"""We have a small dataset of artists and their genres to make it easier for the user to search for a venue by the artist name"""
+"""We turned to information into a dictionary to use in our functions"""
+artist_genre = pd.read_csv("ling 360 proje FINAL_VER top 50 artists genres - Sayfa1.csv")
+artist_data = artist_genre.set_index("Artist")
+artists = list(artist_data.index.values)
+a_genres = list(artist_data["Genre"])
+artist_genre_dict = {}
+for i in range(len(artists)):
+  artist_genre_dict[artists[i]] = a_genres[i]
+
+#artist_genre_dict
+
+"""Our lists of elements which makes it easier to check if the findings in the input matches any items in our lists to classify them."""
+genress = ["r&b", "metal", "rock", "hip-hop", "pop", "electronic", "blues", "jazz", "punk", "k-pop", "latin", "rap"]
+provinces = ["taksim", "beşiktaş", "kadıköy", "beyoğlu", "levent", "bebek", "şişli"]
+venue = list(nightlife_data.index.values)
+
 access_token = "5O6MM3BQBVCZHYIV5OMCFPDOO3NTH3AG"
 person = Wit(access_token)
-person.message('Where can I listen to R&B in Taksim?')
+person.message("Let's go crazy with rock tonight!")
 
-#nightlife_data.loc[["cook"], ["genre"]]
-
-def find_genre(place):
+def find_genre(venue):
   """Finds the genre of a venue"""
-  place = place.lower()
-  return nightlife_data.loc[[place], ["genre"]]
+  venue = venue.lower()
+  return nightlife_data.loc[[venue], ["GENRE"]]
+
+find_genre("kastel")
 
 def find_venue(string):
   """Finds venues given genre or province"""
   string = string.lower()
   if string in genress:
-    return nightlife_data.loc[nightlife_data["genre"] == string]
+    return nightlife_data.loc[nightlife_data["GENRE"] == string]
   elif string in provinces:
-    return nightlife_data.loc[nightlife_data["province"] == string]
-
-def find_venue_gp(string1, string2):
-  """Finds venues given genre and province"""
-  string1 = string1.lower()
-  string2 = string2.lower()
-  if string1 in genress and string2 in provinces:
-    return nightlife_data.loc[(nightlife_data["genre"] == string1) & (nightlife_data["province"] == string2)]
-  """
-  if string1 in genress:
-    return nightlife_data.loc[nightlife_data["genre"] == string1]
-  if string2 in provinces:
-    return [nightlife_data["province"] == string2]
-  """
-
-find_venue_gp("rock", "levent")
+    return nightlife_data.loc[nightlife_data["PROVINCE"] == string]
 
 find_venue("pop")
 
+def find_venue_gp(string1, string2):
+  """Finds venues given both genre and province"""
+  string1 = string1.lower()
+  string2 = string2.lower()
+  if string1 in genress and string2 in provinces:
+    return nightlife_data.loc[(nightlife_data["GENRE"] == string1) & (nightlife_data["PROVINCE"] == string2)]
+  elif string2 in genress and string1 in provinces:
+    return nightlife_data.loc[(nightlife_data["GENRE"] == string2) & (nightlife_data["PROVINCE"] == string1)]
+
+find_venue_gp("electronic", "beyoğlu")
+
 def InputAnalysis(text):
-  """Finds the province and genre from the wit.ai output"""
+  """Extracts the province and genre entities as well as the intent of the input from the wit.ai output"""
+  """It works with the length of the output, first chekcing if the code was able to find the intent and then if there are one or multiple entities"""
   resp = person.message(text)
-  if len(resp["entities"]) == 2:
-    entity0 = list(resp["entities"])[0]
-    value0 = resp["entities"][entity0][0]["value"]
-    entity0 = re.sub(r"\w+:", "", entity0)
-    entity1 = list(resp["entities"])[1]
-    value1 = resp["entities"][entity1][0]["value"]
-    entity1 = re.sub(r"\w+:", "", entity1)
-    return ([entity0, value0, entity1, value1])
-  elif len(resp["entities"]) == 1:
-    entity0 = list(resp["entities"])[0]
-    value0 = resp["entities"][entity0][0]["value"]
-    entity0 = re.sub(r"\w+:", "", entity0)
-    return ([entity0, value0])
-InputAnalysis('Where can I listen rnb?')
+  if len(resp["intents"]) > 0:
+    intents = "intent"
+    intent = resp["intents"][0]["name"]
+    if len(resp["entities"]) == 2:
+      entity0 = list(resp["entities"])[0]
+      value0 = resp["entities"][entity0][0]["value"]
+      entity0 = re.sub(r"\w+:", "", entity0)
+      entity1 = list(resp["entities"])[1]
+      value1 = resp["entities"][entity1][0]["value"]
+      entity1 = re.sub(r"\w+:", "", entity1)
+      return ([intents, intent, entity0, value0, entity1, value1])
+    elif len(resp["entities"]) == 1:
+      entity0 = list(resp["entities"])[0]
+      value0 = resp["entities"][entity0][0]["value"]
+      entity0 = re.sub(r"\w+:", "", entity0)
+      return ([intents, intent, entity0, value0])
+  else:
+    if len(resp["entities"]) == 2:
+      entity0 = list(resp["entities"])[0]
+      value0 = resp["entities"][entity0][0]["value"]
+      entity0 = re.sub(r"\w+:", "", entity0)
+      entity1 = list(resp["entities"])[1]
+      value1 = resp["entities"][entity1][0]["value"]
+      entity1 = re.sub(r"\w+:", "", entity1)
+      return ([entity0, value0, entity1, value1])
+    elif len(resp["entities"]) == 1:
+      entity0 = list(resp["entities"])[0]
+      value0 = resp["entities"][entity0][0]["value"]
+      entity0 = re.sub(r"\w+:", "", entity0)
+      return ([entity0, value0])
+    
 
-message = "I want to listen rock in beşiktaş"
-entity0, value0, entity1, value1 = InputAnalysis(message)
+InputAnalysis('what can I listen in baylo?')
 
-InputAnalysis("rnb in taksim")
+InputAnalysis("where can I listen to pop?")
 
-find_venue_gp(value1, value0)
+InputAnalysis("ritim")
+
+"""List of the variations of the same response sentence with slight alterations for a more enjoyable experience"""
+sentence_list = [
+                 "For {genre}, go to {bars}. All the cool people are there.",
+                 "If you are into {genre}, {bars} is just what you're looking for.", 
+                 "If you want to enjoy {genre}, the best place to go is {bars}. Thank me later.", 
+                 "When I'm in the mood for {genre}, I always go to {bars}.", 
+                 "{bars} is the place where {genre} fans meet. See you there!",
+                 "I really like {genre} too. You can prefer {bars}.",
+                 "Here you go {genre} fan! You can find your kind of music at {bars}.",
+                 "I don't really get why people love {genre}, but I know {bars} might be worth checking out. Have fun!",
+                 "It's tough to find good {genre} music these days, isn't it? Thankfully there are places like {bars}."]
 
 def NightBot(text):
   
-  """bütün fonksiyonları birleştir"""
+  """Taking the input of the user to give the suitable response"""
+  """First transforming the artists into genre with the dictionary"""
+  """The input is then analysed with wit ai to find the intents and entities"""
+  """According to the intent and information of the wit ai output the required function works"""
+  """If there are enough information to work with one function, the response that is randomly chosen from a list of natural sounding sentences."""
+  """The response is printed with the results."""
+  for i in artists:
+    if len(re.findall(i, text)) > 0:
+      text = text + artist_genre_dict[i]
+      text = text.replace(i, " ")
   returnlist = InputAnalysis(text)
-  if len(returnlist) == 4:
-    entity0, value0, entity1, value1 = InputAnalysis(text)
-    result = find_venue_gp(value1, value0)
-    bars = ",".join([str(i) for i in result.index.values])
-    print(f"You can listen to {value1} in {value0} in these bars: {bars}")
-  elif len(returnlist) == 2:
-    entity0, value0 = InputAnalysis(text)
-    result = find_venue(value0)
-    bars = ",".join([str(i) for i in result.index.values])
-    if entity0 =="province":
-      print(f"These are the bars in {value0}: {bars}")
+  try:
+    if returnlist[0] == "intent":
+      try:
+        if len(returnlist) == 6:
+          intents, intent, entity0, value0, entity1, value1 = InputAnalysis(text)
+          if intent == "find_venue":
+            result = find_venue_gp(value1, value0)
+            bars = ", ".join([(str(i)[0].upper()+str(i)[1:]) for i in result.index.values])
+            print(f"The findings for {value1} and {value0} are these bars: {bars}")
+        elif len(returnlist) == 4:
+          intents, intent, entity0, value0 = InputAnalysis(text)
+          if intent == "find_venue":
+            result = find_venue(value0)
+            bars = ", ".join([(str(i)[0].upper()+str(i)[1:]) for i in result.index.values])
+            print(random.choice(sentence_list).format(genre=value0, bars =bars))
+        elif intent == "find_genre":
+          result = find_genre(value0)
+          venue_genre = result["GENRE"].iloc[0]
+          print(f"They have the genre {venue_genre} in {value0}")
+      except:
+        print("Sorry to tell you that, this is out of my scope, for now...")
     else:
-      print(f"You can listen to {value0} in these bars: {bars}")
-    
-while True:
-    input = user_input = st.text_input("label goes here")
-    if input == "no":
-         break
-    NightBot(input)
+      try:
+        if len(returnlist) == 4:
+          entity0, value0, entity1, value1 = InputAnalysis(text)
+          result = find_venue_gp(value1, value0)
+          bars = ", ".join([(str(i)[0].upper()+str(i)[1:]) for i in result.index.values])
+          print(f"The findings for {value1} and {value0} are these bars: {bars}")
+        elif len(returnlist) == 2:
+          entity0, value0 = InputAnalysis(text)
+          result = find_venue(value0)
+          bars = ", ".join([(str(i)[0].upper()+str(i)[1:]) for i in result.index.values])
+          print(random.choice(sentence_list).format(genre=value0, bars =bars))
+      except:
+        print("I am working on myself for now, so that I can answer all your questions, even this one!")
+  except:
+    print("I am not yet capable of understanging and answering this, accept my apologies...")
 
+#NightBot("What is available for rock?") 
+#evaluation metric:
+#percentage of fails
+#percentage of queries
+#successful interactions, customer satisfaction
+
+user_input = input("What do you feel like for a night out?")
+exit = True
+while exit:
+  NightBot(user_input)
+  print("***Write 'exit' to exit the program please***")
+  user_input = input()
+  if user_input == "exit":
+    print("Okay, that was it then, see you later and have lots of fun!")
+    exit = False
+  else:
+    continue
